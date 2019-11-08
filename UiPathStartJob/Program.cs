60e2386c19561484cb94a1131757d99282140808
DrGennadius <zykovgennady@gmail.com>
2019-11-08 12:01:39 +0300

    Transition to a new project type (with .NET Standard and Core)

--- UiPathStartJob/Program.cs
+++ UiPathStartJob/Program.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using UiPathCloudAPISharp;
 
 namespace UiPathCloudAPISharpStartJob
